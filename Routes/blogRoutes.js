4071362528e8391b4d49c437b8486e893ef93148
Rahul Singh <rahuljsrasr1112@gmail.com>
2025-12-11 13:01:10 +0530

    r

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -5,27 +5,27 @@ import {
   deleteBlog,
   getBlogs,
   getBlogById,
+  getBlogsByCategory,
   getBlogBySlug,
   addComment,
   getCommentsByStatus,
   approveComment,
-  rejectComment
+  rejectComment,
 } from "../Controllers/blogController.js";
 
-import { uploadToLocal } from "../Middlewares/uploadMiddleware.js";
+import { uploadToCloudinary } from "../Middlewares/uploadMiddleware.js";
 
 const router = express.Router();
 
-// create multer instance (local disk)
-const upload = uploadToLocal();
+// Cloudinary multer
+const upload = uploadToCloudinary("blogs");
 
-// middleware to accept multiple inner images and one cover image
+// fields
 const uploadBlogImages = upload.fields([
   { name: "images", maxCount: 20 },
   { name: "coverImage", maxCount: 1 },
 ]);
 
-// routes
 router.post("/create", uploadBlogImages, createBlog);
 router.put("/update/:id", uploadBlogImages, updateBlog);
 router.delete("/delete/:id", deleteBlog);
@@ -33,7 +33,7 @@ router.delete("/delete/:id", deleteBlog);
 router.get("/list", getBlogs);
 router.get("/:id", getBlogById);
 router.get("/slug/:slug", getBlogBySlug);
-
+router.get("/category/:category", getBlogsByCategory);
 router.post("/:blogId/comment", addComment);
 router.get("/", getCommentsByStatus);
 router.put("/comment/:id/approve", approveComment);
