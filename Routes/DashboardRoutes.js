e4f26f0df81207358b572f4bc74418c3c87be078
Rahul Singh <rahuljsrasr1112@gmail.com>
2025-12-24 12:03:38 +0530

    r

--- Routes/DashboardRoutes.js
+++ Routes/DashboardRoutes.js
@@ -1,32 +1,30 @@
 import express from "express";
 import { 
-  getSuperAdminDashboard, 
-  getAdminDashboard, 
-  getFreelancerDashboard 
+  getUnifiedDashboard
 } from "../Controllers/DashboardController.js";
 import { authenticateAndAuthorize } from "../Middlewares/AuthMiddleware.js";
 
 const router = express.Router();
-
+router.get("/dashboard", authenticateAndAuthorize(["superadmin", "admin", "freelancer"]), getUnifiedDashboard);
 // Super Admin Route
-router.get(
-  "/super-admin", 
-  authenticateAndAuthorize(["super_admin"]), 
-  getSuperAdminDashboard
-);
+// router.get(
+//   "/super-admin", 
+//   authenticateAndAuthorize(["super_admin"]), 
+//   getSuperAdminDashboard
+// );
 
-// Admin (Salon Owner) Route
-router.get(
-  "/admin", 
-  authenticateAndAuthorize(["admin"]), 
-  getAdminDashboard
-);
+// // Admin (Salon Owner) Route
+// router.get(
+//   "/admin", 
+//   authenticateAndAuthorize(["admin"]), 
+//   getAdminDashboard
+// );
 
-// Freelancer Route
-router.get(
-  "/freelancer", 
-  authenticateAndAuthorize(["freelancer"]), 
-  getFreelancerDashboard
-);
+// // Freelancer Route
+// router.get(
+//   "/freelancer", 
+//   authenticateAndAuthorize(["freelancer"]), 
+//   getFreelancerDashboard
+// );
 
 export default router;
